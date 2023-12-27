1bc15080a5ea2e61c51764d40090813cb3d54604
Tej52 <tejsshiva52@gmail.com>
2023-12-27 21:14:58 +0530

    learning

--- C++code/hello.cpp
+++ C++code/hello.cpp
@@ -1,4 +1,15 @@
 #include<iostream>
+using namespace std;
+void swap(int &a,int &b)
+{
+    int temp;
+    temp = a ;
+    a = b;
+    b = temp;
+}
 int main(){
-    std::cout<<"Hi teja";
+    int a= 5,b=10;
+    swap(a,b);
+    cout<<"in main"<<a<<b;
+    return 0;
 }
