9776e175e53ca69d59a947d2fcb528cc77ab30fd
Tej52 <tejsshiva52@gmail.com>
2024-01-15 00:49:08 +0530

    Ascending Order in C++ code is upload.

--- C++code/hello.cpp
+++ C++code/hello.cpp
@@ -1,18 +1,10 @@
 #include<iostream>
 using namespace std;
-void swap(int &a,int &b)
-{
 
-    printf("%p  %p\n", a, b);
-    int temp;
-    temp = a ;
-    a = b;
-    b = temp;
-}
 int main(){
-    int a= 5, b=10;
-    printf("%p  %p\n", a, b);
-    swap(a, b);
-    cout<<"in main "<<a<<"\t"<<b;
+	char name[20];
+	cout<<"Enter your name: ";
+	cin>>name;
+	cout<<"Hello "<<name;
     return 0;
 }
